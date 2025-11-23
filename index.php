e37dbfa8ed2c9534abf3b2bbebf57f092632fa41
ddickyy <dikiidarmawam05x2@gmail.com>
2025-11-23 15:28:27 +0700

    update

--- index.php
+++ index.php
@@ -136,8 +136,6 @@ function getInitials($name)
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/style-home.css">
-    <link rel="stylesheet" href="css/style-header.css">
-    <link rel="stylesheet" href="css/style-footer.css">
 </head>
 
 <body>
